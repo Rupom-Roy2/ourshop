0b0aa1f2f063130aacc7794d4b1b2c6e7f237f38
Rupom-Roy2 <117434222+Rupom-Roy2@users.noreply.github.com>
2023-05-18 13:49:21 +0600

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,167 @@
+<?php    
+// User Input form Data Count
+function InputCount($col,$value){
+    global $connection;
+    $stm = $connection->prepare("SELECT $col FROM users WHERE $col=?");
+    $stm->execute(array($value));
+    $count=$stm->rowCount(); 
+    return  $count;
+}
+// Get Column Count
+function GetColumnCount($tbl,$col,$value){
+    global $connection;
+    $stm = $connection->prepare("SELECT $col FROM $tbl WHERE $col=?");
+    $stm->execute(array($value));
+    $count=$stm->rowCount(); 
+    return  $count;
+}
+
+// Get Table Data
+function GetTableData($tbl){
+    global $connection;
+    $stm=$connection->prepare("SELECT * FROM $tbl WHERE user_id=?");
+    $stm->execute(array($_SESSION['user']['id']));
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+// Get Single Table Data
+function GetSingleData($tbl,$id){
+    global $connection;
+    $stm=$connection->prepare("SELECT * FROM $tbl WHERE user_id=? AND id=?");
+    $stm->execute(array($_SESSION['user']['id'],$id));
+    $result = $stm->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
+// Delete Table Data
+function DeleteTableData($tbl,$id){
+    global $connection;
+    $stm=$connection->prepare("DELETE FROM $tbl WHERE user_id=? AND id=?");
+    $delete = $stm->execute(array($_SESSION['user']['id'],$id));
+    return $delete;
+}
+
+// Get Product Category Name
+function getProductCategoryName($col,$id){
+    global $connection;
+    $stm=$connection->prepare("SELECT $col FROM categories WHERE id=?");
+    $stm->execute(array($id));
+    $result = $stm->fetch(PDO::FETCH_ASSOC);
+    return $result[$col];
+}
+// Get Product Name
+function getProductName($col,$id){
+    global $connection;
+    $stm=$connection->prepare("SELECT $col FROM products WHERE id=?");
+    $stm->execute(array($id));
+    $result = $stm->fetch(PDO::FETCH_ASSOC);
+    return $result[$col];
+}
+function getTotalProducts(){
+    global $connection;
+    $stm=$connection->prepare("SELECT id FROM products");
+    $stm->execute();
+    $result = $stm->rowCount();
+    return $result;
+}
+// Get Manufacture Name
+function getManufactureName($col,$id){
+    global $connection;
+    $stm=$connection->prepare("SELECT $col FROM manufactures WHERE id=?");
+    $stm->execute(array($id));
+    $result = $stm->fetch(PDO::FETCH_ASSOC);
+    return $result[$col];
+}
+// Get Group Name
+function getGroupName($col,$name,$pid){
+    global $connection;
+    $stm=$connection->prepare("SELECT $col FROM groups WHERE group_name=? AND product_id=?");
+    $stm->execute(array($name,$pid));
+    $result = $stm->fetch(PDO::FETCH_ASSOC);
+    return $result[$col];
+}
+function getGroupNameByID($col,$id,$pid){
+    global $connection;
+    $stm=$connection->prepare("SELECT $col FROM groups WHERE id=? AND product_id=?");
+    $stm->execute(array($id,$pid));
+    $result = $stm->fetch(PDO::FETCH_ASSOC);
+    return $result[$col];
+}
+
+// function getAdmin($id,$col){
+//     global $connection;
+//     $stm=$connection->prepare("SELECT $col FROM admins WHERE id=?");
+//     $stm->execute(array($id));
+//     $result=$stm->fetch(PDO::FETCH_ASSOC);
+//     return $result[$col];
+// }
+
+
+// Login User Profile data
+function getProfile($id){
+    global $connection;
+    $stm=$connection->prepare("SELECT * FROM users WHERE id=?");
+    $stm->execute(array($id));
+    $result=$stm->fetch(PDO::FETCH_ASSOC);
+    return $result; 
+}
+
+function getTotalValue($tbl,$col){ 
+    $total_sales =0;
+    $sales = GetTableData($tbl);
+    foreach($sales as $sale){
+        $total_sales =  $total_sales + $sale[$col];
+    }
+    return $total_sales;
+}
+
+function get_header(){
+    require_once('includes/header.php');
+}
+ 
+
+function get_footer(){
+    require_once('includes/footer.php');
+}
+
+// Admins Functions
+function admin_header(){
+    require_once('admin/header.php');
+}
+function admin_footer(){
+    require_once('admin/footer.php');
+}
+// Get Table Data
+function GetAdminData($tbl){
+    global $connection;
+    $stm=$connection->prepare("SELECT * FROM $tbl");
+    $stm->execute();
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+ 
+
+
+// Send OTP On Mobile Number
+function SendSMS($to,$message){  
+    $token = "XXXXXXXXXXXXXXXXXXXXXX";
+    $url = "http://api.greenweb.com.bd/api.php?json";
+
+    $data= array(
+    'to'=>"$to",
+    'message'=>"$message",
+    'token'=>"$token"
+    ); 
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_ENCODING, '');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $smsresult = curl_exec($ch);
+    $smsresult = json_decode($smsresult,true);
+    $status = $smsresult[0]['status'];
+    return $status;
+    //Error Display
+    // echo curl_error($ch);
+}
---
+++ login.php
@@ -0,0 +1,150 @@
+<?php  
+require_once('config.php');
+session_start();
+
+if(isset($_POST['login_form'])){
+ 
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+ 
+    if(empty($username)){
+        $error = "Username is Required!";
+    }
+    elseif(empty($password)){
+        $error = "Password is Required!";
+    }
+    else{
+        $password = SHA1($password);
+       
+        $loginCount = $connection->prepare("SELECT id,email,mobile,username,password,email_status,mobile_status FROM users WHERE username=? AND password=?");
+        $loginCount->execute(array($username,$password));
+        $userloginCount = $loginCount->rowCount();
+
+        if($userloginCount == 1){
+            $userData =  $loginCount->fetch(PDO::FETCH_ASSOC);
+
+            if($userData['mobile_status'] == 1 AND $userData['email_status'] == 1){
+                $_SESSION['user']  = $userData;
+                header('location:'.GET_APP_URL().'/dashboard');
+            }
+            else{
+                // User Verification Data  
+                $_SESSION['user_email']  = $userData['email'];
+                $_SESSION['user_mobile']  = $userData['mobile'];
+
+                // Generate Email Code
+                $email_code = rand(111111,999999);
+                $stm=$connection->prepare("UPDATE users SET email_code=? WHERE email=?");
+                $stm->execute(array($email_code,$userData['email']));
+               
+                // Generate Mobile Code
+                $mobile_code = rand(111111,999999);
+                $stm=$connection->prepare("UPDATE users SET mobile_code=? WHERE mobile=?");
+                $stm->execute(array($mobile_code,$userData['mobile']));
+
+
+        
+                // Send Email for Verification
+                $message = "Your Verification Code is: ".$email_code;
+                mail($userData['email'],"Email Verification",$message);
+
+                // Send SMS on mobile
+                $sms = "Your Verification Code is: ".$mobile_code;
+                SendSMS($userData['mobile'],$sms);
+                
+                header('location:verification.php');
+            }
+        }
+        else{
+            $error = "Username or Password is Wrong!";
+        }
+    }
+
+}
+
+if(isset($_SESSION['user'])){
+    header('location:'.GET_APP_URL().'/dashboard');
+}
+?>
+<!DOCTYPE html>
+<html class="h-100" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Our Store - Login</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <link href="css/style.css" rel="stylesheet">
+    
+</head>
+
+<body class="h-100">
+    
+    <!--*******************
+        Preloader start
+    ********************-->
+    <div id="preloader">
+        <div class="loader">
+            <svg class="circular" viewBox="25 25 50 50">
+                <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10" />
+            </svg>
+        </div>
+    </div>
+    <!--*******************
+        Preloader end
+    ********************-->
+ 
+    <div class="login-form-bg h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100">
+                <div class="col-xl-6">
+                    <div class="form-input-content">
+                        <div class="card login-form mb-0">
+                            <div class="card-body pt-5">
+                                <a class="text-center" href="index.php"> <h2>Login</h2></a>
+                                <?php if(isset($error)):?>
+                                <div class="alert alert-danger">
+                                    <?php echo $error;  ?>
+                                </div>
+                                <?php endif; ?>
+                                <?php if(isset($success)):?>
+                                <div class="alert alert-success">
+                                    <?php echo $success;  ?>
+                                </div>
+                                <?php endif; ?>
+                                <form  method="POST" action="" class="mt-5 mb-5 login-input">
+                                    <div class="form-group">
+                                        <input type="text" name="username" class="form-control" placeholder="Username">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" name="password" class="form-control" placeholder="Password">
+                                    </div>
+                                    <button type="submit" name="login_form" class="btn login-form__btn submit w-100">Login</button>
+                                </form>
+                                <p class="mt-5 login-form__footer">Dont have account? <a href="registration.php" class="text-primary">Registration</a> now</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+     
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <script src="plugins/common/common.min.js"></script>
+    <script src="js/custom.min.js"></script>
+    <script src="js/settings.js"></script>
+    <script src="js/gleek.js"></script>
+    <script src="js/styleSwitcher.js"></script>
+</body>
+</html>
+
+
+
+
+
